6a9418c3693b3bbc67b47ab90d8e73e68cec6436
Thiago Breder <thiagobreder13@gmail.com>
2025-05-03 22:31:22 -0300

    teste

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def visualizar_produto_request():
     if not name:
         return jsonify({'error': 'O parâmetro "name" é obrigatório.'}), 400
     produto = visualizar_produto(name)
-    return jsonify({'data': produto, 'mensagem': 'Consulta realizada com sucesso.'}), 200
+    return jsonify({'data': produto, 'mensagem': 'Consulta realizada com sim.'}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
